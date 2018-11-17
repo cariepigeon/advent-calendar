c34759e4e396cdaf8a96da200dedcd2125ca71b5
Carie <cariepigeon81@gmail.com>
2018-11-16 22:35:18 -0500

    Create loop to create correct amount of 'closed door' divs.

--- advent.js
+++ advent.js
@@ -7,6 +7,7 @@ var month = now.getMonth();
 //make sure it's december
 /*if (month == 11) {*/
 /*get as many giphys as how many days have passed this month*/
+function giphys() {
 	var base = "http://api.giphy.com/v1/gifs/search?q=elf+christmas&api_key=zQS6Q69ZYfhp91Rwql4VGCufzkzT0yar&limit=" + today + "";
 	var xhr = $.get(base);
 	//var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=elf+christmas&api_key=zQS6Q69ZYfhp91Rwql4VGCufzkzT0yar&limit=30");
@@ -15,26 +16,33 @@ var month = now.getMonth();
 	/*--------------------------------------------*/
 		
 		var jiffs = response.data;
-
 		for (i in jiffs) {
-
 			/*if divdate(from the id) <= today, then do this append*/
 			$("#container").append("<img src='"+jiffs[i].images.original.url+"' style='height:200px; width:200px; '/>");
 		}
 	});
+}
 /*}*/
-	/*for days after today create divs with a number for each and a border for each (closed door look)*/
-	const div = document.querySelector(".next");
-	function attachDivs(div) {
-		/*Add for loop to create new div for each day left until christmas*/
-		let divs = document.createElement('div');
+/*End of getting giphys----------------------------------------------------------------------------------------------------*/
+/*for days after today create divs with a number for each and a border for each (closed door look)*/
+	
+const div = document.querySelector("#container");
+
+function attachDivs(div) {
+//how to make j number of divs
+	for (let j = (today +1); j<26; j++) {
+
+		console.log(j);
+		const divs = document.createElement('div');
 		divs.className='newDiv';
 
-		//content should be 32 - today
-		divs.textContent = (31 - `${today}`);
-		console.log(divs.textContent);
 		divs.style.width = "200px";
 		divs.style.height = "200px";
-		div.appendChild(divs);
+		//make text = appropriate day for each div
+		divs.textContent = [j];
+		div.appendChild(divs)[j];
 	}
-	attachDivs(div);
+}
+
+attachDivs(div);
+giphys();
