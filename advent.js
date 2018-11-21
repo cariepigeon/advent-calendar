b1e9c47186385f64574575d166ca96392af862cf
Carie <cariepigeon81@gmail.com>
2018-11-20 22:51:14 -0500

    add weather api for north pole.

--- advent.js
+++ advent.js
@@ -4,6 +4,8 @@ var today = now.getDate();
 var month = now.getMonth();
 //console.log(today);
 
+//capture input and use that var for the 'base'.
+
 //make sure it's december
 /*if (month == 11) {*/
 /*get as many giphys as how many days have passed this month*/
@@ -18,7 +20,7 @@ function giphys() {
 		var jiffs = response.data;
 		for (i in jiffs) {
 			/*if divdate(from the id) <= today, then do this append*/
-			$("#container").append("<img src='"+jiffs[i].images.original.url+"' style='height:200px; width:200px; '/>");
+			$("#container").append("<img src='"+jiffs[i].images.original.url+"' '/>");
 		}
 	});
 }
@@ -29,20 +31,37 @@ function giphys() {
 const div = document.querySelector("#container");
 
 function attachDivs(div) {
-//how to make j number of divs
+//make 'j' number of divs
 	for (let j = (today +1); j<26; j++) {
 
 		console.log(j);
 		const divs = document.createElement('div');
 		divs.className='newDiv';
-
-		divs.style.width = "200px";
-		divs.style.height = "200px";
-		//make text = appropriate day for each div
 		divs.textContent = [j];
 		div.appendChild(divs)[j];
 	}
 }
 
+//API for getting north pole weather.
+var lat = 90;
+var lon = 20;
+var key = '7986f10690a82af8d4327fa8a81e5f52';
+$.ajax({
+    url: 'https://cors-anywhere.herokuapp.com/http://api.openweathermap.org/data/2.5/weather',
+    dataType: 'json',
+    type: 'GET',
+    data: {lat: lat, lon: lon, appid: key, units: 'imperial'},
+    
+    success: function(data){
+      var wf = '';
+      
+        wf += '<p><b>' + 'North Pole: '+ data.main.temp + '&deg;F ' + '  |  ' + data.weather[0].main + ", " + 
+        data.weather[0].description + '<p>'
+      $(".weather-container").html(wf);
+    }
+  });
+
+
+/*------------------------------------------------*/
 attachDivs(div);
 giphys();
