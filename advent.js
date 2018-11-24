b8ec2e28997a66dceac5df419e9e9c3b87bcf275
Carie <cariepigeon81@gmail.com>
2018-11-23 20:04:16 -0500

    Add 'if' statement for month.

--- advent.js
+++ advent.js
@@ -4,13 +4,11 @@ var today = now.getDate();
 var month = now.getMonth();
 //console.log(today);
 
-//capture input and use that var for the 'base'.
-
 //make sure it's december
 /*if (month == 11) {*/
 /*get as many giphys as how many days have passed this month*/
 function giphys() {
-	var base = "http://api.giphy.com/v1/gifs/search?q=elf+christmas&api_key=zQS6Q69ZYfhp91Rwql4VGCufzkzT0yar&limit=" + today + "";
+	var base = "http://api.giphy.com/v1/gifs/search?q=christmas+story&api_key=zQS6Q69ZYfhp91Rwql4VGCufzkzT0yar&limit=" + today + "";
 	var xhr = $.get(base);
 	//var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=elf+christmas&api_key=zQS6Q69ZYfhp91Rwql4VGCufzkzT0yar&limit=30");
 	xhr.done(function(response) { 
@@ -26,22 +24,28 @@ function giphys() {
 }
 /*}*/
 /*End of getting giphys----------------------------------------------------------------------------------------------------*/
-/*for days after today create divs with a number for each and a border for each (closed door look)*/
-	
+
+/*for days after today create divs with a number for each and a border for each (closed door look)*/	
 const div = document.querySelector("#container");
 
+function notDecember(div) {
+	for (let k = 1; k<26; k++) {
+		const divs = document.createElement('div');
+		divs.className='newDiv';
+		divs.textContent = [k];
+		div.appendChild(divs)[k];
+	}
+}
 function attachDivs(div) {
 //make 'j' number of divs
 	for (let j = (today +1); j<26; j++) {
-
-		console.log(j);
+		//console.log(j);
 		const divs = document.createElement('div');
 		divs.className='newDiv';
 		divs.textContent = [j];
 		div.appendChild(divs)[j];
 	}
 }
-
 //API for getting north pole weather.
 var lat = 90;
 var lon = 20;
@@ -55,13 +59,17 @@ $.ajax({
     success: function(data){
       var wf = '';
       
-        wf += '<p><b>' + 'North Pole: '+ data.main.temp + '&deg;F ' + '  |  ' + data.weather[0].main + ", " + 
-        data.weather[0].description + '<p>'
+        wf += '<p><b>' + 'North Pole: '+ data.main.temp + '&deg;F ' + '  |  ' + data.weather[0].main /*+ ", " + 
+        data.weather[0].description*/ + '<p>'
       $(".weather-container").html(wf);
     }
   });
 
-
 /*------------------------------------------------*/
-attachDivs(div);
-giphys();
+if (month == 11) {
+	giphys();
+	attachDivs(div);
+}
+else {
+	notDecember(div);
+	}
